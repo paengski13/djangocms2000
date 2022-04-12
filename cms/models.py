4a7cd117a8cea3d9073d746f9af074b8bfb4118b
Rafael Torres <rafaeltorres0307@gmail.com>
2022-04-13 10:02:43 +1200

    django3 support

--- cms/models.py
+++ cms/models.py
@@ -8,7 +8,7 @@ from django.contrib.contenttypes.models import ContentType
 from django.core.cache import cache
 from django.core.exceptions import ValidationError
 from django.utils.safestring import mark_safe
-from django.utils.encoding import python_2_unicode_compatible
+from six import python_2_unicode_compatible
 from django.db.models.signals import post_save
 
 from . import settings as cms_settings
