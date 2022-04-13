0a2f0552ee8c60acf067db3ef846f0833551a6c1
Rafael Torres <8873246+paengski13@users.noreply.github.com>
2022-04-13 22:08:49 +1200

    django3 support

--- cms/admin/filters.py
+++ cms/admin/filters.py
@@ -1,7 +1,7 @@
 from datetime import date
 
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from django.contrib.admin import SimpleListFilter
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.sites.models import Site
